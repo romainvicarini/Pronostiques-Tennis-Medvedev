9b9809de20cbe9aeabed80158dde4317507f6de7
Romain <91475053+romainvicarini@users.noreply.github.com>
2022-03-06 23:32:50 +0100

    Update Romain_06MAR2022.R

--- Romain_06MAR2022.R
+++ Romain_06MAR2022.R
@@ -12,28 +12,28 @@ library(glmnet)
 library(rpart)
 library(ggplot2)
 
-# Projet Prédiction Tennis
+# Projet PrÃ©diction Tennis
 
 # Selection du fichier source
 setwd(dir = "C:/Users/AlbanVicarini/OneDrive - Borakay Software/Documents/R")
 
-# Importation des données
+# Importation des donnÃ©es
 
 tennis <- read.table("C:/Users/AlbanVicarini/OneDrive - Borakay Software/Documents/R/atp_matches_2016-2022_Medvedev.csv", header = TRUE, sep = ";", dec = ".")
 
-# Analyse des données de tennis
+# Analyse des donnÃ©es de tennis
 dim(tennis)
 class (tennis)
 summary(tennis)
 
-# On enlève les données incompletes : tailles de joueurs ou classement
+# On enlÃ¨ve les donnÃ©es incompletes : tailles de joueurs ou classement
 
 tennis = na.omit(tennis)
 summary(tennis)
-# on observe avec la fonction ci-dessus que l'on a supprimé 18 lignes ou l'on n'avait pas de valeur
+# on observe avec la fonction ci-dessus que l'on a supprimÃ© 18 lignes ou l'on n'avait pas de valeur
 
-# si on veut faire une régression logistique, il est important de transfromer les valeus qualitatives en facteurs (ex Y, surface,nom des opposants, etc. )
-# on voit pas exemple que Y est considéré comme un entier dans la classe et non comme un facteur
+# si on veut faire une rÃ©gression logistique, il est important de transfromer les valeus qualitatives en facteurs (ex Y, surface,nom des opposants, etc. )
+# on voit pas exemple que Y est considÃ©rÃ© comme un entier dans la classe et non comme un facteur
 class(tennis$Y)
 tennis$Y <- as.factor(tennis$Y)
 tennis$Opponents <- as.factor(tennis$Opponents)
@@ -42,7 +42,7 @@ tennis$O_hand <- as.factor(tennis$O_hand)
 # on essaie de regarder la taille moyenne des jouers de tennis
 mean(tennis$O_ht)
 
-# on essaie de restituer un arbre de décision entre les victoires et les défaites
+# on essaie de restituer un arbre de dÃ©cision entre les victoires et les dÃ©faites
 # on doit utiliser le package RPART et RPART plot
 
 arbre <- rpart(Y~., data = tennis, cp=0.02)
@@ -58,10 +58,10 @@ tennis2<- tennis[,-4] # on supprime lla colonne du nom de l'opposant de Medvedev
 tennis2
 arbre2 <- rpart(Y~., data = tennis2, cp=0.02)
 rpart.plot(arbre2, main="Match Medvedev sans les noms")
-# le résultat est bien plus représentatif
+# le rÃ©sultat est bien plus reprÃ©sentatif
 
 
-# on veut maintenant prédire la victoire ou la défaite de Medvedev
+# on veut maintenant prÃ©dire la victoire ou la dÃ©faite de Medvedev
 # On va diviser notre fichier en 2: l'un pour l'apprentissage 80% et l'autre pour le test 20%
 
 mod <- glm(Y~., data = tennis2, family = 'binomial')
@@ -80,10 +80,14 @@ apply(RES,2,monerreur,y=RES$Y)
 dim(RES)
 class(RES)
 summary(mod)
-# les étoiles permettent de voir quelles sont celles qui influent dans le résultat: 3 étoiles pour le classement et la date du tournoi et 1 étoile pour la surface.
-# il n'y a qu'un match par jour ce qui explique 3 étoiles pour le résultat.
+# les Ã©toiles permettent de voir quelles sont celles qui influent dans le rÃ©sultat: 3 Ã©toiles pour le classement et la date du tournoi et 1 Ã©toile pour la surface.
+# il n'y a qu'un match par jour ce qui explique 3 Ã©toiles pour le rÃ©sultat.
 
 
 # visuel: on installe le package ggplot2
 library("ggplot2")
 ggplot(tennis2)+aes(x=tennis$O_rank, y=tennis2$O_age)+geom_point(bins=40)
+
+
+
+
